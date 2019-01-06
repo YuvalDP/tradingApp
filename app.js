7d15985a50488e6286d9bc90062f91d029105556
laljiDP <laljiprajapati572@gmail.com>
2019-01-06 18:30:06 +0530

    created API signup, update credentials.

--- app.js
+++ app.js
@@ -4,16 +4,18 @@ var path = require('path');
 require('dotenv').config();
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('core-js')
 
 var indexRouter = require('./routes/index');
 const authRoute = require('./routes/authRoute');
+const usersRoute = require('./routes/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -22,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/auth', authRoute);
-// app.use('/users', usersRouter);
+app.use('/users', usersRoute);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
